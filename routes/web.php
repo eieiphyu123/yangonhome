eef18b98f5cf31e5e27a890eaa12bbdee3dd5117
eieiphyu123 <miss.eiphyu@gmail.com>
2024-07-17 19:21:34 +0630

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,3 @@ Route::group(['middleware'=>['auth','role:Admin'],'prefix'=>'backend', 'as'=>'ba
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
